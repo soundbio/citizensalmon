8e9e6d3efae93b3e1037d513206d8bcd17797da2
Paul S <pauls@ptse.org>
2015-11-04 06:30:18 -0800

    genpop -> genepop. Add pickled data and example usage of Population in main.py.

--- PcaSycData/main.py
+++ PcaSycData/main.py
@@ -1,9 +1,10 @@
-﻿from genpop_data import GenPopData
+﻿from genepop_data import GenePopData
+from population import Population
 
-# example use of GenPopData
+# example use of GenePopData and Population
 
-# read and process data in GenPop file 'genepop_western_alaska_chinook_RAD.txt'
-gpd = GenPopData("genepop_western_alaska_chinook_RAD.txt")
+# read and process data in GenePop file 'genepop_western_alaska_chinook_RAD.txt'
+gpd = GenePopData("genepop_western_alaska_chinook_RAD.txt")
 
 fishies = gpd.fishies()                     # list of all fish (and alleles) in file
 fishies = gpd.fishies(gpd.popnames()[2])    # list of all fish (and alleles) in third population
@@ -11,3 +12,17 @@ fishies = gpd.fishies(gpd.popnames()[2])    # list of all fish (and alleles) in 
 alleles = gpd.alleles()                     # all alleles of all populations
 alleles = gpd.alleles(gpd.popnames()[2])    # all alleles in third population
 
+#save to pickle file
+gpd.toFile('alaska_chinook.pickle')
+
+
+# once data are pickled, we no longer need GenePopData 
+# to retrieve data from the original data file
+apop = Population(None)
+apop.fromFile('alaska_chinook.pickle')
+
+fishies = apop.fishies()                    # list of all fish (and alleles) in file
+fishies = apop.fishies(apop.popnames()[2])  # list of all fish (and alleles) in third population
+
+alleles = apop.alleles()                   	# all alleles of all populations
+alleles = apop.alleles(apop.popnames()[2]) 	# all alleles in third population
