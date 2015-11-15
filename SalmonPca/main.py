2267e3460df00c6b247e00f17cbb06d599e1ef55
Paul Schnackenberg <pauls@jetstreamsoftware.com>
2015-11-15 04:57:03 -0800

    Update main.py

--- SalmonPca/main.py
+++ SalmonPca/main.py
@@ -2,7 +2,6 @@
 lib_path = os.path.abspath(os.path.join('..', 'PcaSycData'))
 sys.path.append(lib_path)
 
-from genepop_data import GenePopData
 from pca_synth_data import PcaSynthData
 from pca_alleles import AllelesPCA
 from population import Population
@@ -10,25 +9,29 @@ from population import Population
 if False:
     psd = PcaSynthData(5, 3.5, 2, 800)
     psd.test()
-elif False:
-    # read and process data in GenePop file 'genepop_western_alaska_chinook_RAD.txt'
-    gpd = GenePopData("genepop_western_alaska_chinook_RAD.txt")
-    gpd.toFile('alaska_chinook.pickle')
-    apop = Population(None)
-    apop.fromFile('alaska_chinook.pickle')
-
-    fishies = gpd.fishies()                     # list of all fish (and alleles) in file
-    afishies = apop.fishies()
-    fishies = gpd.fishies(gpd.popnames()[2])    # list of all fish (and alleles) in third population
-    afishies = apop.fishies(apop.popnames()[2])
-
-    alleles = gpd.alleles()                     # all alleles of all populations
-    alleles = gpd.alleles(gpd.popnames()[2])    # all alleles in third population
 elif True:
     apop = Population(None)
-    apop.fromGenePop("genepop_western_alaska_chinook_RAD.txt")
-    apop.toFile('alaska_chinook.pickle')
+
+    # read and process data in GenePop file 'genepop_western_alaska_chinook_RAD.txt'
+    #apop.fromGenePop("genepop_western_alaska_chinook_RAD.txt")
+
+    #save to pickle file
+    #apop.toFile('alaska_chinook.pickle')
+
+    # once data are pickled, we no longer need GenePopData 
+    # to retrieve data from the original data file
     apop.fromFile('alaska_chinook.pickle')
+
+    """
+    # get fish information by population
+    fishies = apop.fishies()                    # list of all fish (and alleles) in file
+    fishies = apop.fishies(apop.popnames()[2])  # list of all fish (and alleles) in third population
+
+    # get alleles by population
+    alleles = apop.alleles()                   	# all alleles of all populations
+    alleles = apop.alleles(apop.popnames()[2]) 	# all alleles in third population
+    """
+
     apca = AllelesPCA(apop)
     mean = apca.popmean(apop.popnames()[2])
     fst = apca.fst(apop.popnames()[2], None)
